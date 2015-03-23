b26a91b2e14cc794866cb523de6cab32b40ced2a
funnythingz <funnythingz69+github@gmail.com>
2015-03-23 23:07:05 +0900

    added set_area

--- lib/bijint.rb
+++ lib/bijint.rb
@@ -2,18 +2,20 @@ require "bijint/version"
 
 module Bijint
   class Bijint
-
-    def now(area = 'tokyo')
-      url(area, Time.now)
+    def now
+      url(set_area, Time.now)
     end
 
-    def lgtm(area = 'tokyo')
-      "[![bijint](#{url(area, Time.now)})](#{url('tokyo', Time.now)})"
+    def lgtm
+      "[![bijint](#{url(set_area, Time.now)})](#{url('tokyo', Time.now)})"
     end
 
     def url(area, time)
       "http://www.bijint.com/assets/pict/#{area}/pc/#{time.strftime('%H%M')}.jpg"
     end
 
+    def set_area
+      %w(tokyo).shuffle.first
+    end
   end
 end
