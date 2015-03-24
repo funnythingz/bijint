56cc7653d17b0fef3995fd04863b2263ba3c6bae
funnythingz <funnythingz69+github@gmail.com>
2015-03-24 22:36:07 +0900

    Fixed spec

--- spec/bijint_spec.rb
+++ spec/bijint_spec.rb
@@ -2,26 +2,26 @@ require 'spec_helper'
 
 describe Bijint do
 
-  describe 'now' do
-    let(:bijint) {
-      Bijint::Bijint.new
-    }
+  describe 'method' do
+    let(:bijint) { Bijint::Bijint.new }
 
-    it {
+    it 'now' do
       time = Time.now
       expect(bijint.now).to eq "http://www.bijint.com/assets/pict/tokyo/pc/#{time.strftime('%H%M')}.jpg"
-    }
-  end
+    end
 
-  describe 'lgtm' do
-    let(:bijint) {
-      Bijint::Bijint.new
-    }
+    it 'random' do
+      expect(bijint.random).to match /http:\/\/www\.bijint\.com\/assets\/pict\/tokyo\/pc\/\d{4}\.jpg/
+    end
 
-    it {
+    it 'lgtm' do
       time = Time.now
       expect(bijint.lgtm).to eq "[![bijint](http://www.bijint.com/assets/pict/tokyo/pc/#{time.strftime('%H%M')}.jpg)](http://www.bijint.com/assets/pict/tokyo/pc/#{time.strftime('%H%M')}.jpg)"
-    }
+    end
+
+    it 'lgtm_random' do
+      expect(bijint.lgtm_random).to match /\[!\[bijint\]\(http:\/\/www\.bijint\.com\/assets\/pict\/tokyo\/pc\/\d{4}\.jpg\)\]\(http:\/\/www\.bijint\.com\/assets\/pict\/tokyo\/pc\/\d{4}\.jpg\)/
+    end
   end
 
 end
